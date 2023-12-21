607eae9ad1e5d2954f2b73a3e995e71c0b10d55f
Jon Ashdown <jonashdown@users.noreply.github.com>
2023-12-21 17:22:37 +0000

    Thu 21 Dec 2023 17:22:37 GMT

--- functions/carbon-intensity-ingest/carbon-intensity-ingest.test.js
+++ functions/carbon-intensity-ingest/carbon-intensity-ingest.test.js
@@ -4,6 +4,8 @@ import { jest } from '@jest/globals';
 const RestoreDate = Date.now;
 
 const mockFetch = jest.spyOn(global, 'fetch');
+const { stringify } = JSON;
+const mockJsonStringify = jest.spyOn(JSON, 'stringify');
 
 const { env } = process;
 
@@ -60,7 +62,8 @@ beforeAll(() => {
 })
 
 beforeEach(() => {
-  mockFetch.mockResolvedValue({ json: jest.fn(() => mockData) });
+  mockFetch.mockResolvedValue({ json: jest.fn(() => mockData), status: 200, statusText: 'ok' });
+  mockJsonStringify.mockImplementation(stringify);
 });
 
 afterEach(() => {
@@ -100,33 +103,47 @@ describe('Carbon Intensity Ingest', () => {
 
     it('sends metrics to Grafana', async () => {
       await handler();
+
+      //testing dates is hard, and the body of fetch cant be a json object, so check JSON.stringify is called correctly, but omitting the time
+      //Then check that fetch body is the return value of the last JSON.stringify call
+      //N.B might need to keep track of the Nth value
+      expect(JSON.stringify).toBeCalledWith(
+        expect.arrayContaining([
+          expect.objectContaining({ interval: 1800, name: 'intensity', tags: ['type=intensity', 'postcode=RG10', 'data-source=carbonintensity.intensity'], value: 266, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'gas', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 43.6, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'coal', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 0.7, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'biomass', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 4.2, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'nuclear', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 17.6, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'hydro', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 2.2, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'imports', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 6.5, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'other', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 0.3, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'wind', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 6.8, time: expect.any(Number) }),
+          expect.objectContaining({ interval: 1800, name: 'solar', tags: ['type=generation', 'postcode=RG10', 'data-source=carbonintensity.generation'], value: 18.1, time: expect.any(Number) })
+        ])
+      );
+
       expect(fetch).toBeCalledWith(
         process.env.GRAFANA_API,
         expect.objectContaining({
           headers: { Authorization: `Bearer ${process.env.GRAFANA_USER_ID}:${process.env.GRAFANA_API_KEY}`, 'Content-Type': 'application/json' },
           method: 'post',
-          // body: expect.stringContaining('{ "interval": 1800, "name": "carbonintensity.intensity", "tags": ["type = intensity", "postcode = RG10", "data-source=carbonintensity.intensity"], "value": 266 }')
+          body: mockJsonStringify.mock.results.findLast(({ type }) => type==='return').value
         })
-        // {
-        //   data: [
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.intensity', tags: ['type=intensity', 'postcode=RG10'], value: 266 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.gas', tags: ['type=generation', 'postcode=RG10'], value: 43.6 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.coal', tags: ['type=generation', 'postcode=RG10'], value: 0.7 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.biomass', tags: ['type=generation', 'postcode=RG10'], value: 4.2 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.nuclear', tags: ['type=generation', 'postcode=RG10'], value: 17.6 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.hydro', tags: ['type=generation', 'postcode=RG10'], value: 2.2 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.imports', tags: ['type=generation', 'postcode=RG10'], value: 6.5 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.other', tags: ['type=generation', 'postcode=RG10'], value: 0.3 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.wind', tags: ['type=generation', 'postcode=RG10'], value: 6.8 }),
-        //     expect.objectContaining({ interval: 1800, name: 'carbonintensity.generation.solar', tags: ['type=generation', 'postcode=RG10'], value: 18.1 })
-        //   ],
-        //   headers: { Authorization: `Bearer ${process.env.GRAFANA_USER_ID}:${process.env.GRAFANA_API_KEY}`, 'Content-Type': 'application/json' }
-        // }
       )
     });
 
     it('throws an error when sending metrics fails', async () => {
-      mockFetch.mockRejectedValue()
+      const thrownError = new Error('Grafana broke');
+      mockFetch.mockResolvedValueOnce({ json: jest.fn(() => mockData), status: 200, statusText: 'ok' }).mockRejectedValue(thrownError);
+      let caughtError;
+      try {
+        await handler();
+      } catch (error) {
+        caughtError = error;
+      } finally {
+        expect(caughtError).toEqual(thrownError);
+        expect(fetch).toBeCalledTimes(2);
+      }
     });
   });
 });
